47c977ac798eb1d0992880a0eed328eedb4681f9
yannis94 <yannisbgci@proton.me>
2023-06-16 16:45:46 +0200

    add a init sql script to db

---
+++ database/init.sql
@@ -0,0 +1,49 @@
+CREATE TABLE IF NOT EXISTS admin (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(80),
+    last_name VARCHAR(80),
+    email VARCHAR(120),
+    password varchar(150),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS client (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(80),
+    last_name VARCHAR(80),
+    email VARCHAR(120),
+    password varchar(150),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS account (
+    id SERIAL PRIMARY KEY,
+    client_id INTEGER REFRENCES user(id) ON DELETE CASCADE,
+    number UUID,
+    balance INTEGER, 
+    created_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS tansfer (
+    id SERIAL PRIMARY KEY,
+    demand_id INTEGER REFERENCES transfer_demand(id),
+    done BOOLEAN,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS transfer_demand (
+    id SERIAL PRIMARY KEY,
+    closed BOOLEAN,
+    from_account UUID,
+    to_account UUID,
+    message VARCHAR(250),
+    amount INTEGER,
+    accepted BOOLEAN,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS closed_account (
+    id SERIAL PRIMARY KEY,
+    number UUID,
+    created_at TIMESTAMP,
+);
